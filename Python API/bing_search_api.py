4287c4556260b09a05373a409c8093f9e39c0ee0
NixonThuo <thuonixon808@gmail.com>
2017-04-05 10:54:04 -0700

    Commit for API

---
+++ Python API/bing_search_api.py
@@ -0,0 +1,19 @@
+
+import requests
+import json
+
+
+def bing_search(query):
+    url = 'https://api.cognitive.microsoft.com/bing/v5.0/search'
+    # query string parameters
+    payload = {'q': query}
+    # custom headers
+    headers = {'Ocp-Apim-Subscription-Key': 'aef765d25f7c46298ce2fe5d8b4773a5'}
+    # make GET request
+    data = requests.get(url, params=payload, headers=headers)
+    # get JSON response
+    data = json.loads(data.text)
+    return data
+
+
+print bing_search('Andela')
